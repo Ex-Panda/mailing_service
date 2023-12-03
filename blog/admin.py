842b08cc028ef903af15ce10b9169153e618b16c
panda <frausonya131@gmail.com>
2023-12-03 22:17:42 +0300

    commit readme and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'date_creation', 'is_publication',)
+    list_filter = ('date_creation',)
+    search_fields = ('title',)
+    fields = ('title', 'content', 'preview', 'is_publication')
+
